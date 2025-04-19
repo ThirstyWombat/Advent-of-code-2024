67b2f8f64dd5dc81dbce7f122089451c25572487
Vincent Baraga <vincentb.baraga@gmail.com>
2025-04-19 14:04:04 -0400

    final solutions for challenge 1 part 1 and 2

--- challenge1.js
+++ challenge1.js
@@ -1,43 +1,74 @@
-//Step #1:
-// create a function that sorts through a given array of numbers and creates a new array with the numbers sorted from lowest to highest
-//use that function for both inputs and save each output into a new variable (array of arrays?)
-//Step #2:
-//create a function that loops through the array of arrays
-//New Plan:
-//Seperate the the parsed input into two arrays by filtering by odd and even indexes, then sort the arrays so the elements range from lowest to highest.
+function exampleTest() {
+  let results = [];
 
-// let exampleList1 = [3,4,2,1,3,3];
-// let exampleList2 = [4,3,5,3,9,3];
+  let exampleList1 = [3, 4, 2, 1, 3, 3];
+  let exampleList2 = [4, 3, 5, 3, 9, 3];
+
+  let exampleList1SORTED = exampleList1.sort(function (a, b) {
+    return a - b;
+  });
+
+  let exampleList2SORTED = exampleList2.sort(function (a, b) {
+    return a - b;
+  });
+  for (let i = 0; i < exampleList1SORTED.length; i++) {
+    results.push(exampleList1SORTED[i] - exampleList2SORTED[i]);
+  }
+  console.log(results.reduce((a, b) => a + b, 0));
+}
+// Acquires the input from the input.txt file, maps it into an array, parses it into integers, then filters out any remaining empty spaces
 const fs = require("fs");
 const data = fs.readFileSync("input.txt", "utf8");
 const lines = data.split(/\s+/);
-// console.log(lines);
-let sortedLines = lines.map((str) => parseInt(str));
-// console.table(sortedLines);
 
-let testArray = [14000, 100, 90, 20];
+let sortedLines = lines.map((str) => parseInt(str));
+
+let sortedLinesFiltered = sortedLines.filter(Boolean);
 
 function findTotalSummedValue() {
-  const column1 = sortedLines.filter((el, idx) => idx % 2 === 0);
+  //Splits the initial array into two arrays filtered by odd and even indexes
+  const column1 = sortedLinesFiltered.filter((el, idx) => idx % 2 === 0);
 
-  const column2 = sortedLines.filter((el, idx) => idx % 2 !== 0);
-
+  const column2 = sortedLinesFiltered.filter((el, idx) => idx % 2 !== 0);
+  // Sorts all elements in both arrays from lowest integer to highest
   const sortedColumn1 = column1.sort(function (a, b) {
     return a - b;
   });
   const sortedColumn2 = column2.sort(function (a, b) {
     return a - b;
   });
-  console.log(sortedColumn2);
-  // let exampleList1 = [3,4,2,1,3,3];
-  // let exampleList2 = [4,3,5,3,9,3];
+
+  let comparison = [];
+
+  //for loop solution part 1:
+  for (let i = 0; i < sortedColumn1.length; i++) {
+    let difference = sortedColumn1[i] - sortedColumn2[i];
+    if (difference < 0) {
+      difference = difference * -1;
+    }
+    comparison.push(difference);
+  }
+  //for loop solution part 2:
+  let results = [];
+  for (let i = 0; i < sortedColumn1.length; i++) {
+    let leftNumber = sortedColumn1[i];
+    let count = 0;
+
+    for (let j = 0; j < sortedColumn2.length; j++) {
+      if (leftNumber === sortedColumn2[j]) {
+        count = count + 1;
+      }
+    }
+
+    results.push(leftNumber * count);
+  }
+
+  let totalSummedValue = comparison.reduce((a, b) => a + b, 0);
+
+  let part2Summed = results.reduce((a, b) => a + b, 0);
+
+  console.log("total summed value Part 1>", totalSummedValue);
+  console.log("Part 2 summed >", part2Summed);
 }
-
+// exampleTest();
 findTotalSummedValue();
-// function sortList(){
-//     console.log(testArray.sort(function(a,b){
-//         return a-b;
-//     }));
-// }
-
-// sortList();
